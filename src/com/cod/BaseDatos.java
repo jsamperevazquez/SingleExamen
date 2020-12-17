b1a309a9ab0af17d3cef2ed179a7504c65090fff
Jose Angel Sampere <jsamperevazquez@danielcastelao.org>
2020-12-17 19:18:13 +0100

    Javadoc creado en el código

--- src/com/cod/BaseDatos.java
+++ src/com/cod/BaseDatos.java
@@ -1,11 +1,24 @@
 package com.cod;
 
+/**
+ * @author angel
+ * @version 1.0
+ * Clase para crear métodos de comprobar acceso
+ */
 public class BaseDatos {
-
+    /**
+     * Correo electrónico
+     */
     String correoElectronico = "";
+    /**
+     * Ip del servidor
+     */
     String ipServidor = "127.0.0.2";
 
     // Creo mi unica instancia  y la inicializo en null
+    /**
+     * Unica instacia  con Singleton
+     */
     private static BaseDatos instance = null;
 
     // Cambio el constructor a privado para poder bloquearlo.
@@ -14,6 +27,11 @@ public class BaseDatos {
         correoElectronico = "anonymous@danielcastelao.org";
     }
  // Creo un método que comprueba si instance es null, si no es null ya fe creado y lo devuelve
+
+    /**
+     * Método para comprobar si una instancia es nula y lo es la crea.
+     * @return retorna la instancia
+     */
     public static BaseDatos getInstance() {
         if (instance == null){
             instance = new BaseDatos();
@@ -23,6 +41,10 @@ public class BaseDatos {
         return instance;
     }
 
+    /**
+     * Método para permitir o no la conexión a la base de datos.
+     * @return retorna 1 o 0
+     */
     public boolean permitirConexion() {
         if (correoElectronico != "anonymous@danielcastelao.org") {
             return true;
--- src/com/cod/Usuarios.java
+++ src/com/cod/Usuarios.java
@@ -1,5 +1,10 @@
 package com.cod;
 
+/**
+ * @author angel
+ * @version 1.0
+ * Clase para instanciar objetos de tipos usuario en BaseDatos
+ */
 public class Usuarios {
     public static void main(String[] args) {
         if (conectarbasedatos1("pepe@danielcastelao.org")) {
@@ -15,6 +20,11 @@ public class Usuarios {
         ;
     }
 
+    /**
+     * Metodo para crear usuario y comprobar conexión
+     * @param usuario Recibe un usario como parámetro
+     * @return retorna un bool valido o fallo
+     */
     public static boolean conectarbasedatos1(String usuario) {
         // Creo los usuarios con el método estático de la subclase getInstance.
         BaseDatos usuario1 = BaseDatos.getInstance();
@@ -22,6 +32,10 @@ public class Usuarios {
         return usuario1.permitirConexion();
     }
 
+    /**
+     * Mismo que el anterior pero con otro usuario
+     * @return
+     */
     public static boolean conectarBaseDatos2() {
         BaseDatos usuario2 = BaseDatos.getInstance();
         System.out.println("Conectando a " + usuario2.ipServidor + ", con el usuario " + usuario2.correoElectronico);
